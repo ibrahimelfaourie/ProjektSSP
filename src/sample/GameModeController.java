aac38d801ef6b8ef5944120c3c1e2fc3ab18ef4d
ibrahimelfaourie <56521013+ibrahimelfaourie@users.noreply.github.com>
2020-03-07 13:00:01 +0100

    Utmana friend och request

---
+++ src/sample/GameModeController.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class GameModeController {
+
+    int player1;
+    int player2;
+
+    public void setPlayers(int playerOne, int playerTwo){
+
+        player1 = playerOne;
+        player2 = playerTwo;
+
+
+    }
+}
